e24f2be3af2cf7fc88a78295f116ef98d0832bcf
Yaroslav Vorontsov <darth.yarius@gmail.com>
2016-08-01 13:30:43 +0300

    Added missing header files

--- libextobjc/libextobjc.h
+++ libextobjc/libextobjc.h
@@ -17,10 +17,13 @@ FOUNDATION_EXPORT const unsigned char libextobjcVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <libextobjc/PublicHeader.h>
 
 
+#import <libextobjc/metamacros.h>
 #import <libextobjc/EXTADT.h>
 #import <libextobjc/EXTConcreteProtocol.h>
+#import <libextobjc/EXTCoroutine.h>
 #import <libextobjc/EXTKeyPathCoding.h>
 #import <libextobjc/EXTNil.h>
+#import <libextobjc/EXTRuntimeExtensions.h>
 #import <libextobjc/EXTSafeCategory.h>
 #import <libextobjc/EXTScope.h>
 #import <libextobjc/EXTSelectorChecking.h>
